e13d43de99ffdb8377676f84d8add5bcf2e024dc
Sahil191104 <sahilrupareliya95@gmail.com>
2023-08-05 22:46:17 +0530

    05-08-2023(Redux-Context)

--- startup-website/src/user/components/Header.js
+++ startup-website/src/user/components/Header.js
@@ -10,7 +10,7 @@ function Header(props) {
             {/* Spinner Start */}
             {/* <div id="spinner" className="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
                 <div className="spinner" />
-            </div> */}
+            </div> */} 
             {/* Spinner End */}
 
             {/* Topbar Start */}
